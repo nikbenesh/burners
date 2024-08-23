3aacb0f4065ccfca0405f11b16aa99e2c93bc185
Kresh <andrey@symbiotic.fi>
2024-08-23 21:10:33 +0400

    fix: chainsecurity - DoS for ETHx Burner

---
+++ src/contracts/burners/ETHx_Burner.sol
@@ -0,0 +1,87 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+import {SelfDestruct} from "src/contracts/SelfDestruct.sol";
+import {UintRequests} from "src/contracts/UintRequests.sol";
+
+import {IETHx_Burner} from "src/interfaces/burners/ETHx/IETHx_Burner.sol";
+import {IStaderConfig} from "src/interfaces/burners/ETHx/IStaderConfig.sol";
+import {IStaderStakePoolsManager} from "src/interfaces/burners/ETHx/IStaderStakePoolsManager.sol";
+import {IUserWithdrawalManager} from "src/interfaces/burners/ETHx/IUserWithdrawalManager.sol";
+
+import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
+import {Multicall} from "@openzeppelin/contracts/utils/Multicall.sol";
+
+contract ETHx_Burner is UintRequests, Multicall, IETHx_Burner {
+    using Math for uint256;
+    using EnumerableSet for EnumerableSet.UintSet;
+
+    /**
+     * @inheritdoc IETHx_Burner
+     */
+    address public immutable COLLATERAL;
+
+    /**
+     * @inheritdoc IETHx_Burner
+     */
+    address public immutable STADER_CONFIG;
+
+    /**
+     * @inheritdoc IETHx_Burner
+     */
+    address public immutable USER_WITHDRAW_MANAGER;
+
+    /**
+     * @inheritdoc IETHx_Burner
+     */
+    address public immutable STAKE_POOLS_MANAGER;
+
+    constructor(address collateral, address staderConfig) {
+        COLLATERAL = collateral;
+
+        STADER_CONFIG = staderConfig;
+        USER_WITHDRAW_MANAGER = IStaderConfig(STADER_CONFIG).getUserWithdrawManager();
+        STAKE_POOLS_MANAGER = IStaderConfig(STADER_CONFIG).getStakePoolManager();
+
+        IERC20(COLLATERAL).approve(USER_WITHDRAW_MANAGER, type(uint256).max);
+    }
+
+    /**
+     * @inheritdoc IETHx_Burner
+     */
+    function triggerWithdrawal(uint256 maxWithdrawalAmount) external returns (uint256 requestId) {
+        uint256 maxETHWithdrawAmount = IStaderConfig(STADER_CONFIG).getMaxWithdrawAmount();
+        if (
+            IStaderStakePoolsManager(STAKE_POOLS_MANAGER).previewWithdraw(maxWithdrawalAmount) > maxETHWithdrawAmount
+                || IStaderStakePoolsManager(STAKE_POOLS_MANAGER).previewWithdraw(maxWithdrawalAmount + 1)
+                    <= maxETHWithdrawAmount
+        ) {
+            revert InvalidHints();
+        }
+
+        requestId = IUserWithdrawalManager(USER_WITHDRAW_MANAGER).requestWithdraw(
+            Math.min(IERC20(COLLATERAL).balanceOf(address(this)), maxWithdrawalAmount), address(this)
+        );
+
+        _addRequestId(requestId);
+
+        emit TriggerWithdrawal(msg.sender, requestId);
+    }
+
+    /**
+     * @inheritdoc IETHx_Burner
+     */
+    function triggerBurn(uint256 requestId) external {
+        _requestIds.remove(requestId);
+
+        IUserWithdrawalManager(USER_WITHDRAW_MANAGER).claim(requestId);
+
+        new SelfDestruct{value: address(this).balance}();
+
+        emit TriggerBurn(msg.sender, requestId);
+    }
+
+    receive() external payable {}
+}
